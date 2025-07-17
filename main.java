b9de6899c9cee76d4a859aa88e21e15590b7a6c5
aryanmaurya-prog <mauryaaryan147@gmail.com>
2025-07-17 20:22:50 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
